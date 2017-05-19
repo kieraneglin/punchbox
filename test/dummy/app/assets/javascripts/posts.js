c35a28f17c55b9790329583518653c0ae25b02f6
Kieran Eglin <kieran.eglin@gmail.com>
2017-05-18 23:57:55 -0700

    added boilerplate JS

--- test/dummy/app/assets/javascripts/posts.js
+++ test/dummy/app/assets/javascripts/posts.js
@@ -1,8 +1,9 @@
-class Posts extends PunchBox {
+class Posts {
   // This is a rough-draft for the DSL I'd like to create.
   // Things will change, especially when it comes to declaring a class
-  
+
   constructor() {
+    console.log('PB');
     // Class setup.  Not strictly related to PunchBox
   }
 
