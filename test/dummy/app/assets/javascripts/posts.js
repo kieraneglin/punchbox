1bb9d872c78cc195c055d16d8bec4b006f69e3dc
Kieran Eglin <kieran.eglin@gmail.com>
2017-05-18 22:45:03 -0700

    Mocked hypothetical DSL

---
+++ test/dummy/app/assets/javascripts/posts.js
@@ -0,0 +1,16 @@
+class Posts extends PunchBox {
+  // This is a rough-draft for the DSL I'd like to create.
+  // Things will change, especially when it comes to declaring a class
+  
+  constructor() {
+    // Class setup.  Not strictly related to PunchBox
+  }
+
+  controller() {
+    // Code for every action of this controller
+  }
+
+  index() {
+    // Code for Index
+  }
+}
